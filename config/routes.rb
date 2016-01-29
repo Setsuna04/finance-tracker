91350f5c7418fbf31e8a3b3623c8d408e8d4a791
setsuna04 <Setsuna04@gmail.com>
2016-01-29 00:26:36 +0000

    Add stocks and ajax for UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
 
 root :to => 'welcome#index'
+get 'my_portfolio', to: "users#my_portfolio"
+get 'search_stocks', to: "stocks#search"
 
 
   # You can have the root of your site routed with "root"
